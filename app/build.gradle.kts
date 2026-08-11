d6c4e0d0527ff90e4a1446eeb3018ab9299b1d2b
ohmingyu <omg5626@naver.com>
2026-08-12 06:01:48 +0900

    1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.mygallery"
         minSdk = 30
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.3"
+        versionCode = 8
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
